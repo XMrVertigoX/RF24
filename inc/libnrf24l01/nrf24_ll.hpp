6d13bf637bd2fdab97684f9fcff3c6d78d6903ff
Caspar Friedrich <c.s.w.friedrich@gmail.com>
2018-11-03 16:42:16 +0100

    Prepare for protected

--- inc/libnrf24l01/nrf24_ll.hpp
+++ inc/libnrf24l01/nrf24_ll.hpp
@@ -29,24 +29,6 @@ public:
 
   /*
    * ##########################################################################
-   * Commands
-   * ##########################################################################
-   */
-
-  uint8_t R_REGISTER(nRF24_Register reg, uint8_t bytes[], uint8_t numBytes = 1);
-  uint8_t W_REGISTER(nRF24_Register reg, const uint8_t bytes[], uint8_t numBytes = 1);
-  uint8_t R_RX_PAYLOAD(uint8_t bytes[], uint8_t numBytes);
-  uint8_t W_TX_PAYLOAD(const uint8_t bytes[], uint8_t numBytes);
-  uint8_t FLUSH_TX();
-  uint8_t FLUSH_RX();
-  uint8_t REUSE_TX_PL();
-  uint8_t R_RX_PL_WID(uint8_t& payloadLength);
-  uint8_t W_ACK_PAYLOAD(uint8_t pipe, const uint8_t bytes[], uint8_t numBytes);
-  uint8_t W_TX_PAYLOAD_NOACK(const uint8_t bytes[], uint8_t numBytes);
-  uint8_t NOP();
-
-  /*
-   * ##########################################################################
    * Register modification
    * ##########################################################################
    */
@@ -114,6 +96,25 @@ public:
   void enableDataPipe(uint8_t pipe, bool enable = true);
   void enableAutoAcknowledgment(uint8_t pipe, bool enable = true);
 
+  /*
+   * ##########################################################################
+   * Commands
+   * ##########################################################################
+   */
+
+  uint8_t R_REGISTER(nRF24_Register reg, uint8_t bytes[], uint8_t numBytes = 1);
+  uint8_t W_REGISTER(nRF24_Register reg, const uint8_t bytes[], uint8_t numBytes = 1);
+  uint8_t R_RX_PAYLOAD(uint8_t bytes[], uint8_t numBytes);
+  uint8_t W_TX_PAYLOAD(const uint8_t bytes[], uint8_t numBytes);
+  uint8_t FLUSH_TX();
+  uint8_t FLUSH_RX();
+  uint8_t REUSE_TX_PL();
+  uint8_t R_RX_PL_WID(uint8_t& payloadLength);
+  uint8_t W_ACK_PAYLOAD(uint8_t pipe, const uint8_t bytes[], uint8_t numBytes);
+  uint8_t W_TX_PAYLOAD_NOACK(const uint8_t bytes[], uint8_t numBytes);
+  uint8_t NOP();
+
+protected:
   /*
    * ##########################################################################
    * Utility functions
